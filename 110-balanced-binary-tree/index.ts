eff633f3106146ef2882166048670de37b2cc54d
ryan-zheng <ryanzheng1998@gmail.com>
2022-11-06 23:15:32 +0800

    add second solution

--- 110-balanced-binary-tree/index.ts
+++ 110-balanced-binary-tree/index.ts
@@ -12,24 +12,54 @@ class TreeNode {
 }
 
 const isBalanced = (root: TreeNode | null): boolean => {
+  const helper = (
+    root: TreeNode | null
+  ): { isBalanced: boolean; height: number } => {
+    if (root === null) return { isBalanced: true, height: 0 }
+    const left = helper(root.left)
+    if (left.isBalanced === false) return { isBalanced: false, height: 0 }
+    const right = helper(root.right)
+    if (right.isBalanced === false) return { isBalanced: false, height: 0 }
+
+    const isBalanced = Math.abs(left.height - right.height) <= 1
+
+    if (isBalanced === false) return { isBalanced: false, height: 0 }
+
+    return {
+      isBalanced: true,
+      height: Math.max(left.height, right.height) + 1,
+    }
+  }
+
   return helper(root).isBalanced
 }
 
-const helper = (
-  root: TreeNode | null
-): { isBalanced: boolean; height: number } => {
-  if (root === null) return { isBalanced: true, height: 0 }
-  const left = helper(root.left)
-  if (left.isBalanced === false) return { isBalanced: false, height: 0 }
-  const right = helper(root.right)
-  if (right.isBalanced === false) return { isBalanced: false, height: 0 }
+const isBalanced2 = (root: TreeNode | null): boolean => {
+  if (root === null) return true
+  const memo = new Map<TreeNode, number>()
 
-  const isBalanced = Math.abs(left.height - right.height) <= 1
-
-  if (isBalanced === false) return { isBalanced: false, height: 0 }
-
-  return {
-    isBalanced: true,
-    height: Math.max(left.height, right.height) + 1,
+  const getDepth = (
+    node: TreeNode | null,
+    memo: Map<TreeNode, number>
+  ): number => {
+    if (!node) {
+      return 0
+    }
+    if (memo.has(node)) {
+      return memo.get(node)!
+    }
+    const depth =
+      Math.max(getDepth(node.left, memo), getDepth(node.right, memo)) + 1
+    memo.set(node, depth)
+    return depth
   }
+
+  const leftDepth = getDepth(root.left, memo)
+  const rightDepth = getDepth(root.right, memo)
+
+  return (
+    Math.abs(leftDepth - rightDepth) <= 1 &&
+    isBalanced(root.left) &&
+    isBalanced(root.right)
+  )
 }
