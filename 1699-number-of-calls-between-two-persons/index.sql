6ddd39591b4578ebfa0831161ebb8202c49d0118
ryan-zheng <ryanzheng1998@gmail.com>
2022-11-25 20:34:15 +0800

    progress

--- 1699-number-of-calls-between-two-persons/index.sql
+++ 1699-number-of-calls-between-two-persons/index.sql
@@ -1,2 +1,7 @@
 select
-    p.
+    if(from_id < to_id, from_id, to_id) as person1,
+    if(from_id > to_id, from_id, to_id) as person2,
+    count(*) as call_count,
+    sum(duration) as total_duration
+from
+    calls
